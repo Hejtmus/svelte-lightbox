745fe5536666280c846850a437199b703d5ec632
filip <filip.holcik.official@gmail.com>
2021-05-23 16:45:01 +0200

    Added before style.

--- src/Modal/ModalCover.svelte
+++ src/Modal/ModalCover.svelte
@@ -31,6 +31,13 @@
         align-items: center;
         justify-content: center;
     }
+    div::before {
+        content: '';
+        position: absolute;
+        top: 0; bottom: 0; left: 0; right: 0;
+        opacity: 0;
+        z-index: -1;
+    }
     div::after {
         content: "";
         clear: both;
