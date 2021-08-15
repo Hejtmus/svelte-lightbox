22bf44a821d9c29d039b36eafecad8babde16218
filip <filip.holcik.official@gmail.com>
2021-08-15 23:56:05 +0200

    Fixed prefix object literal that caused error when bundled using vite

--- src/Modal/presets.js
+++ src/Modal/presets.js
@@ -1,25 +1,27 @@
-export  default {
-    "expand": {
+const presets =  {
+    expand: {
         "width": "100%",
         "maxWidth": "",
         "height": "auto",
         "maxHeight": ""
     },
-    "fit": {
+    fit: {
         "width": "",
         "maxWidth": "80vw",
         "height": "",
         "maxHeight": "80vh"
     },
-    "fullscreen": {
+    fullscreen: {
         "width": "100vw",
         "maxWidth": "100vw",
         "height": "100vh",
         "maxHeight": "100vh"
     },
-    "scroll": {
+    scroll: {
         "width": "auto",
         "height": "auto",
         "overflow": "scroll"
     }
 }
+
+export default presets;
