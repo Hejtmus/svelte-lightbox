f0b4bc83f4dfa6d3b4c3f12d1e2b272b97842c64
Filip Holčík <filip.holcik.official@gmail.com>
2022-04-22 23:26:55 +0200

    Styled docs

--- docs/src/App.svelte
+++ docs/src/App.svelte
@@ -1,38 +1,47 @@
 <script>
+	import {
+		Row,
+		Col
+	} from "sveltestrap";
 	import { Lightbox } from '../../dist';
-	export let name;
 </script>
 
 <main>
-	<Lightbox description="Simple lightbox">
-		<img src="./img/test1.png" alt="Simple lightbox">
-	</Lightbox>
-	<Lightbox description="Simple lightbox">
-		<img src="./img/test2.png" alt="Simple lightbox">
-	</Lightbox>
-	<Lightbox description="Simple lightbox">
-		<img src="./img/test3.png" alt="Simple lightbox">
-	</Lightbox>
+	<Row>
+		<Col>
+			<h1>
+				svelte-lightbox demo
+			</h1>
+		</Col>
+	</Row>
+	<Row>
+		<Col xs="12">
+			<Lightbox description="Simple lightbox" preset="fit">
+				<img src="./img/test1.png" alt="Simple lightbox" style="max-width: 100%">
+			</Lightbox>
+		</Col>
+	</Row>
+	<Row>
+		<Col>
+			<Lightbox description="Simple lightbox" preset="expand">
+				<img src="./img/test2.png" alt="Simple lightbox" style="max-height: 85vh">
+			</Lightbox>
+		</Col>
+	</Row>
+	<Row>
+		<Col>
+			<Lightbox description="Simple lightbox" preset="fit">
+				<img src="./img/test3.png" alt="Simple lightbox">
+			</Lightbox>
+		</Col>
+	</Row>
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		max-width: 100%;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
