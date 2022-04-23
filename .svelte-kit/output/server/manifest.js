457fb470576e4ba271ef4432d79e0e393b60cd3b
Filip Holčík <filip.holcik.official@gmail.com>
2022-04-23 14:46:37 +0200

    Disabled Jekyll for GH pages and set kit.prerender.default to true

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -1,9 +1,9 @@
 export const manifest = {
 	appDir: "_app",
-	assets: new Set(["bootstrap.css","bootstrap.css.map","bootstrap.min.css","bootstrap.min.css.map","favicon.png","img/test1.png","img/test2.png","img/test3.png"]),
+	assets: new Set([".nojekyll","bootstrap.css","bootstrap.css.map","bootstrap.min.css","bootstrap.min.css.map","favicon.png","img/test1.png","img/test2.png","img/test3.png"]),
 	mimeTypes: {".css":"text/css",".map":"application/json",".png":"image/png"},
 	_: {
-		entry: {"file":"start-78856340.js","js":["start-78856340.js","chunks/index-93e59907.js","chunks/index-df832915.js"],"css":[]},
+		entry: {"file":"start-d854f25f.js","js":["start-d854f25f.js","chunks/index-93e59907.js","chunks/index-df832915.js"],"css":[]},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js'),
