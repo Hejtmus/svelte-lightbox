079eb3b8cfc0ff87fac7db940c15b325d9c366e8
Filip Holčík <filip.holcik.official@gmail.com>
2022-07-18 18:49:20 +0200

    Refactored Lightbox.svelte and LightboxThumbnail.svelte

--- src/lib/Lightbox.svelte
+++ src/lib/Lightbox.svelte
@@ -1,77 +1,77 @@
 <script>
-    import Thumbnail from './LightboxThumbnail.svelte';
-    import Modal from './Modal/Index.svelte';
-    import InternalGallery from './Gallery/InternalGallery.svelte';
-    import BodyChild from "./Modal/BodyChild.svelte";
-    import {onMount} from 'svelte';
+	import Thumbnail from './LightboxThumbnail.svelte'
+    import Modal from './Modal/Index.svelte'
+    import InternalGallery from './Gallery/InternalGallery.svelte'
+    import BodyChild from './Modal/BodyChild.svelte'
+    import { onMount } from 'svelte'
 
-    //exporting classes, for passing classes into thumbnail
-    export let thumbnailClasses = '';
-    export let thumbnailStyle = '';
-    //exporting classes, for passing classes into wrapper
-    export let modalClasses = '';
-    export let modalStyle = '';
-    //array with image descriptions
-    export let gallery = false;
-    //getting universal title and descriptions
-    export let title = '';
-    export let description = '';
-    //exporting duration of fade transition
-    export let transitionDuration = 500;
-    //bool that enables drag n drop protection
-    export let protect = false;
-    //enables other image than in slot
-    export let image = {};
-    //enables portrait mode
-    export let portrait = false;
-    //disables scrolling <body>
-    export let noScroll = true;
-    export let thumbnail = false;
-    export let imagePreset = false;
-    export let clickToClose = false;
-    export let closeButton = true;
+    // exporting classes, for passing classes into thumbnail
+    export let thumbnailClasses = ''
+    export let thumbnailStyle = ''
+    // exporting classes, for passing classes into wrapper
+    export let modalClasses = ''
+    export let modalStyle = ''
+    // array with image descriptions
+    export let gallery = false
+    // getting universal title and descriptions
+    export let title = ''
+    export let description = ''
+    // exporting duration of fade transition
+    export let transitionDuration = 500
+    // bool that enables drag n drop protection
+    export let protect = false
+    // enables other image than in slot
+    export let image = {}
+    // enables portrait mode
+    export let portrait = false
+    // disables scrolling <body>
+    export let noScroll = true
+    export let thumbnail = false
+    export let imagePreset = false
+    export let clickToClose = false
+    export let closeButton = true
 
-    export let isVisible = false;
+    export let isVisible = false
 
-    let modalClicked = false;
+    let modalClicked = false
 
     const toggle = () => {
-        isVisible = !isVisible;
+        isVisible = !isVisible
         toggleScroll()
-    };
+    }
 
     const close = () => {
-        isVisible = false;
+        isVisible = false
         toggleScroll()
-    };
+    }
 
     const coverClick = () => {
         // console.log('coverClick')
         if (!modalClicked || clickToClose) {
             close()
         }
-        modalClicked = false;
+        modalClicked = false
     }
 
     const modalClick = () => {
         // console.log('modalClick')
-        modalClicked = true;
+        modalClicked = true
     }
 
     let toggleScroll = () => {
-    };
+    }
 
     onMount(() => {
-        let defaultOverflow = document.body.style.overflow;
+        const defaultOverflow = document.body.style.overflow
         toggleScroll = () => {
             if (noScroll) {
                 if (isVisible) {
-                    document.body.style.overflow = 'hidden';
+                    document.body.style.overflow = 'hidden'
                 } else {
-                    document.body.style.overflow = defaultOverflow;
+                    document.body.style.overflow = defaultOverflow
                 }
             }
-        };
+        }
     })
 </script>
 
