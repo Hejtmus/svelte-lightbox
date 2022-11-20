daba81cd6b7956299bced7e931d59a1dcb64201c
Filip Holčík <filip.holcik.official@gmail.com>
2022-11-20 10:23:45 +0100

    Removed package directory from VCS

--- package/i18n.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Writable } from 'svelte/store';
-import type { I18n } from './Types';
-declare const i18n: Writable<I18n>;
-export default i18n;
--- package/i18n.js
+++
@@ -1,7 +0,0 @@
-import { writable } from 'svelte/store';
-const i18n = writable({
-    generateLocalizedGalleryCounter: (activeImage, imageCount) => {
-        return `Image ${activeImage + 1} of ${imageCount}`;
-    }
-});
-export default i18n;
